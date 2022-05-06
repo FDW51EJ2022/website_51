32790ed4094b7efa3ed4799c26dba9e1562cec73
Dana Valeria Ramon Saavedra <danasaavedra2109@gmail.com>
2022-05-06 18:00:12 -0500

    js2

--- danavaleria21/js/js2.js
+++ danavaleria21/js/js2.js
@@ -2,7 +2,26 @@ function sumar(){
     let num1 = document.getElementById("num1").value;
     /* document.write(num1); */
     /* alert(num1); */
+    let num2 = document.getElementById("num2").value;
+
+    let suma = 0;
+
+    let resultado = document.getElementById("resultado");
+    let mensaje = document.getElementById("mensaje");
+
+    if (num1.length == 0 || num2.length == 0){
+        mensaje.innerText = "Ingrese los números por favor";
+    }
+    else {
+        /* Si hay datos */
+        if (isNaN(num1) || isNaN(num2)){
+            mensaje.innerText = "Solo números";
+        }
+        else {
+            /* Realizar el cálculo */
+            mensaje.innerText = "";
+            suma = Number(num1) + Number(num2);
+            resultado.value = suma;
+        }
+    }
 }
-
-
-
