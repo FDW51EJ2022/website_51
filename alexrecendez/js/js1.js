9358463af76ba5fd5b8c6a914f0c6993d2822a5b
Alejandra Recendez Rivera <alejandra.recendizrv@uanl.edu.mx>
2022-05-06 21:26:10 -0500

    Fin clase 1 js

--- alexrecendez/js/js1.js
+++ alexrecendez/js/js1.js
@@ -21,6 +21,8 @@ document.write(`El tipo de dato false es: ${typeof false} <br>`);
 document.write(`Comparar 5 contra 6: ${5 == 6} <br>`);
 document.write(`Operador ternario: ${1 == 2 ? true : false} <br>`);
 
+
+
 /* Práctica de variables */
 let sueldo;
 document.write(`Sueldo: ${sueldo} <br>`);
@@ -33,15 +35,16 @@ document.write(`Grupo: ${nombre} <br>`);
 const factor = 1.15;
 document.write(factor + "<br>");
 
-/* Arreglos y objetos en javascript */
 
+
+/* Arreglos y objetos en javascript */
 let lenguajes = ["python", "javascript", "java", "C#"];
 document.write(`Lenguajes: ${lenguajes} <br>`);
 document.write(`Segundo lenguaje: ${lenguajes[1]} <br>`);
 console.log(lenguajes);
 
 let empleado = {
-  nombre: "Jesus Cortez",
+  nombre: "Alejandra Rivera",
   edad: 21,
   lenguajes: lenguajes,
 };
@@ -52,6 +55,8 @@ document.write(`Nombre: ${empleado["nombre"]} <br>`);
 /* let estado = prompt("¿Cúal es su estado de procedencia?");
 let carrera = prompt("¿Cúal es tu carrera?"); */
 
+
+
 /* Estructuras condicionales */
 let total = 1500;
 let descuento = 0;
@@ -61,6 +66,8 @@ if (total > 1000) {
 } else {
   descuento = total * 0.02;
 }
+
+
 
 /* Operador ternario */
 descuento = total > 1000 ? total * 0.1 : total * 0.02;
@@ -77,8 +84,9 @@ if (promedio == 100) {
   document.write(`Bien <br>`);
 }
 
-/* Estructura switch */
 
+
+/* Estructura switch */
 let n1 = 5;
 let n2 = 9;
 let operacion = "+";
@@ -101,7 +109,9 @@ switch (operacion) {
     break;
 }
 
-/* Estructuras repetititvas FOR */
+
+
+/* Estructuras repetititvas for */
 document.write(`Javascript <br>`);
 document.write(`Javascript <br>`);
 document.write(`Javascript <br>`);
@@ -123,11 +133,66 @@ for (let n = 10; n >= 1; n--) {
 
 let tabla = 3;
 for (let n = 1; n <= 10; n++) {
-  document.write(`${tabla}X${n}=${tabla * n} <br>`);
+  document.write(`${tabla} X ${n} = ${tabla * n} <br>`);
 }
 
-/* Ejemplo de estructura DO */
+
+
+/* Ejemplo de estructura do */
 let iteracion = 1;
 do {
   document.write(`do-while <br>`);
 } while (iteracion <= 3);
+
+
+
+/* Declaración de una función */
+/* Sintaxis 1. Como constante */
+
+const saludo = function () {
+  document.write("<h1>Hola</h1>");
+};
+
+const cuadrado = function (num) {
+  return num * num;
+};
+
+/* Llamar a la funcion */
+saludo();
+document.write(`El cuadrado es: ${cuadrado(3)} <br>`);
+
+/* Sintaxis 2. Utilizando function */
+function numeros() {
+  for (let n = 1; n <= 10; n++) {
+    document.write(`${n} <br>`);
+  }
+}
+function cuadrado_v2(num) {
+  return num * num;
+}
+/* Llama a la función */
+numeros();
+document.write(`Cuadrado v2 ${cuadrado_v2(8)} <br>`);
+
+/* Sintaxis 3. Función flecha */
+const cuadrado_v3 = (num) => {
+  return num * num;
+};
+/* LLama a la función */
+document.write(`Cuadrado v3 ${cuadrado_v3(5)} <br>`);
+
+/* Sintaxis 4. Función flecha reducida */
+const cuadrado_v4 = (num) => num * num;
+/* Llama a la función */
+document.write(`Cuadrado v4 ${cuadrado_v4(3)} <br>`);
+
+
+
+/* Manipulando elemento html */
+let p = document.getElementById("info");
+
+console.log(p.textContent);
+console.log(p.innerHTML);
+console.log(p.outerHTML);
+
+p.innerHTML = "Adiós <strong> :D </strong>";
